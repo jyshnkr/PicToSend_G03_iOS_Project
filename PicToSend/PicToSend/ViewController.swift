6b87df927bec6aba9ec4c6efba53a6ad4595eda3
JayaShankar M <jaysh309@gmail.com>
2021-12-06 11:52:08 -0600

    Project Initial Setup

---
+++ PicToSend/PicToSend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicToSend
+//
+//  Created by JayaShankar Mangina on 12/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
