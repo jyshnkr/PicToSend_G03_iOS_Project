a46a8c6c59db3cd5924dcc5e12337aef704d2c38
Goutham6172 <77602631+Goutham6172@users.noreply.github.com>
2021-12-06 15:55:13 -0600

    added collection view

--- PicToSend/PicToSend/ViewController.swift
+++ PicToSend/PicToSend/ViewController.swift
@@ -21,6 +21,20 @@ class ViewController: UIViewController {
         
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+
+        return cell
+    }
+
     
     @objc func importPicture() {
         let picker = UIImagePickerController()
